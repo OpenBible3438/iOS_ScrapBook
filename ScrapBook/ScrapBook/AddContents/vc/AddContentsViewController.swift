9506d09362650b3e03ceeaf60f78755dede8e6c7
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-10-16 21:34:13 +0900

    create AddController

---
+++ ScrapBook/ScrapBook/AddContents/vc/AddContentsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddContentsViewController.swift
+//  ScrapBook
+//
+//  Created by 편성경 on 2022/10/16.
+//
+
+import UIKit
+
+class AddContentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
